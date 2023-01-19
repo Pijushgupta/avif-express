1def536c89e6d24e0e41d0bb006329e529f79567
Pijush Gupta <Pijush@live.com>
2023-01-19 12:43:06 +0530

    change-log: 01.20.2023

--- avif-express.php
+++ avif-express.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
  * Author: Pijush Gupta
  * Author uri: https://www.linkedin.com/in/pijush-gupta-php/
  * Description: Converts Images to AVIF and serve them
- * Version: 2023.01.19
+ * Version: 2023.01.20
  * Tags: avif, images, performance
  */
 
@@ -14,16 +14,56 @@ if (!file_exists(__DIR__ . '/core/app/app.php')) return;
 
 function initiate_plugin() {
 	/**
-	 * TODO: Check php version
+	 * Normally wordpress will not allow plugin to installed from repo if "Requires PHP" not meet.
+	 * But anyone can download and install the plugin to bypass "Requires PHP"
+	 * Checking php version, since only php 8.1 having imageavif() function
+	 */
+	if (version_compare(phpversion(), '8.1.0', '>=') == false) return;
+
+	/**
+	 * text domain, can be used to translate string. But we are using vue js powered admin page.
+	 * So it little to no use. Keeping it for future.
 	 */
 	if (!defined('AVIFE_TEXT_DOMAIN')) define('AVIFE_TEXT_DOMAIN', 'avif_text_domain');
+
+	/**
+	 * plugin's admin page html title
+	 */
 	if (!defined('AVIFE_ADMIN_MENU_TITLE')) define('AVIFE_ADMIN_MENU_TITLE', 'Avif Express');
+
+	/**
+	 * Left hand side menu name 
+	 */
 	if (!defined('AVIFE_ADMIN_MENU_NAME')) define('AVIFE_ADMIN_MENU_NAME', 'Avif Express');
+
+	/**
+	 * This is the slug to used in url of admin page of the plugin 
+	 */
 	if (!defined('AVIFE_SPA_SLUG')) define('AVIFE_SPA_SLUG', 'avif-express');
+
+	/**
+	 * this to load css and js 
+	 */
 	if (!defined('AVIFE_REL')) define('AVIFE_REL', plugins_url('', __FILE__));
+
+	/**
+	 * this to find css, js and php files
+	 */
 	if (!defined('AVIFE_ABS')) define('AVIFE_ABS', plugin_dir_path(__FILE__));
+
+	/**
+	 * vue js root div name, which we are going to target via vue js code
+	 * if you want change this to something else, please also change the same thing in app.js in assets/src directory. 
+	 */
 	if (!defined('AVIFE_VUE_ROOT_ID')) define('AVIFE_VUE_ROOT_ID', 'avife-root');
 
+	/**
+	 * loading the main app code 
+	 */
 	require_once __DIR__ . 	'/core/app/app.php';
 }
+
+/**
+ * Loading everything on plugin_loaded action 
+ */
 add_action('plugins_loaded', 'initiate_plugin');
--- core/app/app.php
+++ core/app/app.php
@@ -1,21 +1,49 @@
 <?php
 if (!defined('ABSPATH')) exit;
+
+/**
+ * Loading auto-loader(composer) 
+ */
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Avife\backend\Ui;
 use Avife\backend\Enqueue;
 use Avife\Routes;
 use Avife\common\Image;
-
 use Avife\frontend\Html;
 
+/**
+ * backend code
+ */
 if (is_admin()) {
+	/**
+	 * creating backend UI
+	 */
 	Ui::activate();
+
+	/**
+	 * Adding css and JS to Backend UI
+	 */
 	Enqueue::do();
+
+	/**
+	 * adding/enabling ajax(non REST) routes
+	 */
 	Routes::enable();
+
+	/**
+	 * Loading Image conversion class
+	 * reason to loading initially to hook it with 'wp_generate_attachment_metadata' and 'delete_attachment'
+	 */
 	Image::activate();
 }
 
+/**
+ * frontend code
+ */
 if (!is_admin()) {
+	/**
+	 * initializing frontend for url alteration 
+	 */
 	Html::init();
 }
