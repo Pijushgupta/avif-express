dea2f9c0e65ea8cabdaa18f034830bc081cee6cb
Pijush <pijush@live.com>
2025-08-29 20:40:30 +0530

    updated mix config for simple processing

--- core/app/backend/webpack.mix.js
+++ core/app/backend/webpack.mix.js
@@ -1,13 +1,6 @@
 const path = require("path");
 let mix = require("laravel-mix");
-mix.setResourceRoot("../dist");
-mix.copyDirectory('./fonts', 'assets/dist/fonts');
+
+
 mix.js("assets/src/app.js", "assets/dist").vue();
 mix.postCss("assets/src/app.css", "assets/dist", [require("tailwindcss")]);
-mix.webpackConfig({
-    watchOptions: {
-        ignored: [
-            path.resolve(__dirname, "assets/dist"),
-        ],
-    },
-});
